768affbf7bf7914ce0883efd24a253faf0db4b65
Morgan Dean <morgandeanhi@gmail.com>
2019-07-26 14:42:22 -1000

    updated for hotjar

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 const G_TRACKING_ID = 'UA-143960290-1';
-
+const HOTJAR_ID = 1419537
 (function($){
   $(function(){
 
@@ -14,3 +14,13 @@ const G_TRACKING_ID = 'UA-143960290-1';
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+(function(h,o,t,j,a,r){
+	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+	h._hjSettings={hjid:HOTJAR_ID,hjsv:6};
+	a=o.getElementsByTagName('head')[0];
+	r=o.createElement('script');r.async=1;
+	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+	a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
